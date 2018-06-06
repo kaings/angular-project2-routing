5002434fbe254cca1fb3606ce41c86a9073f6ccc
gr33ngr33n <actfun07@gmail.com>
2018-06-06 23:30:21 +0800

    additional info: ngOnDestroy to unsubscribe the subscription

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private paramSubscription: Subscription) { }
 
   ngOnInit() {
     this.user = {
@@ -17,12 +18,16 @@ export class UserComponent implements OnInit {
       name: this.activatedRoute.snapshot.params['name']
     };
 
-    this.activatedRoute.params.subscribe(
+    this.paramSubscription = this.activatedRoute.params.subscribe(
       (params: Params) => {
         this.user.id = params['id'];
         this.user.name = params['name'];
       }
     );
+  }
+
+  ngOnDestroy() {
+    this.paramSubscription.unsubscribe();   // this line added in ngOnDestroy to make sure this observable is cleared when component is destroyed (performance improvement)
   }
 
 
