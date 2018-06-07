633eb4c744d7fc7d874fcbb6034eb3048b682642
willys_tjiawi <willys@huubap.com>
2018-06-07 12:00:49 +0800

    fetch queryParams & fragment

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
 
 import { ServersService } from '../servers.service';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, OnDestroy {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  queryParamsSubscription: Subscription;
+  fragmentSubscription: Subscription;
+
+  constructor(private serversService: ServersService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    console.log('----- Fetch queryParams & fragment on Init -----');
+    console.log(this.activatedRoute.snapshot.queryParams);  // fetch query params on Init
+    console.log(this.activatedRoute.snapshot.fragment);   // fetch fragment on Init
+
+    console.log('----- Fetch queryParams & fragment on value changed detected -----');
+    console.log(this.queryParamsSubscription = this.activatedRoute.queryParams.subscribe());  // value is at _subscriptions > [] > _subject > _value
+    console.log(this.fragmentSubscription = this.activatedRoute.fragment.subscribe());    // value is at _subscriptions > [] > _subject > _value
+
+    // this.activatedRoute.queryParams.subscribe( (params: Params) => { console.log(params); } );
+    // this.activatedRoute.fragment.subscribe( (params: string) => { console.log(params); } );
+
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
@@ -24,4 +40,8 @@ export class EditServerComponent implements OnInit {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
   }
 
+  ngOnDestroy() {
+    this.queryParamsSubscription.unsubscribe();
+    this.fragmentSubscription.unsubscribe();
+  }
 }
